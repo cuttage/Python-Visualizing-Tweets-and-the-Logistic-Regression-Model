6622e55ba8b418fe54023f54de09b148f0883edf
cuttage <martafattori@protonmail.ch>
2023-05-09 15:31:07 +0200

    Load the NLTK sample dataset & fix SSL bug

--- main.py
+++ main.py
@@ -6,5 +6,28 @@ import matplotlib.pyplot as plt     # Library for visualization
 import numpy as np                  # Library for math functions
 
 from utils import process_tweet, build_freqs # Our functions for NLP
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
 nltk.download('twitter_samples')
+
+# select the set of positive and negative tweets
+all_positive_tweets = twitter_samples.strings('positive_tweets.json')
+all_negative_tweets = twitter_samples.strings('negative_tweets.json')
+
+tweets = all_positive_tweets + all_negative_tweets ## Concatenate the lists.
+labels = np.append(np.ones((len(all_positive_tweets),1)), np.zeros((len(all_negative_tweets),1)), axis = 0)
+
+# split the data into two pieces, one for training and one for testing (validation set)
+train_pos  = all_positive_tweets[:4000]
+train_neg  = all_negative_tweets[:4000]
+
+train_x = train_pos + train_neg
+
+print("Number of tweets: ", len(train_x))
